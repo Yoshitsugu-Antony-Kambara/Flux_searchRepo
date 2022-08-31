f55496a12b9dbcfcbe206e3ab3d3a43363a43767
Yoshitsugu-Antony-Kambara <tsugu1999213@yahoo.co.jp>
2022-08-31 11:13:52 +0900

    first commit

---
+++ Flux_SearchRepo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flux_SearchRepo
+//
+//  Created by 神原良継 on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
